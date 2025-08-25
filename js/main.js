c5b3aba31cf5d2664dd9292f73a95dc7304abbc2
cor36 <edgaedigah@gmail.com>
2025-08-25 05:47:20 +0300

    Final

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//Work still need to be done on the js file
+//For example, form validation and interactivity
+
+//At the moment, this file is just a placeholder
+//to show that there is a js file linked to the html files
+ 
+//The php one shall do just fine for form handling
